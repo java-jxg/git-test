357fa27694a0767eef6e8b13fef5484c9f1de72a
java_jxg <java_jxg@163.com>
2021-08-19 16:46:44 +0800

    second commit

--- src/main/java/com/atguigu/controller/TestController.java
+++ src/main/java/com/atguigu/controller/TestController.java
@@ -1,7 +1,20 @@
 package com.atguigu.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
     public static void main(String[] args) {
         System.out.println("hello git!");
     }
+
+    @GetMapping("/")
+    public String test(){
+        return "hello";
+    }
+
+
+
 }
